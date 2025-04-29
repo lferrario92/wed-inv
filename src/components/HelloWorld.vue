131e996142ccf7612c748f1819fa007f6d7fb78f
lferrario <lferrario@kiusys.com>
2025-04-28 22:08:51 -0300

    base files

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold mb-4 mt-4">
+      Hello world!
+    </h1>
+    <h2 class="my-1">
+      Font awesome <i class="fas fa-check"></i>
+    </h2>
+    <h2 class="my-1">
+      Tailwind <i class="fas fa-pen"></i>
+    </h2>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
